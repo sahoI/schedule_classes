3e7a7ef67c8db6b2b4cc260b13e2b3a2228bf834
sahoI <igamishao@gmail.com>
2018-09-05 01:38:37 +0900

    ルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'wethreapp/index' => 'wethreapp#index'
+  get 'wethreapp/show' => 'wethreapp#show'
+  get 'wethreapp/scraipe' => 'wethreapp#scraipe'
+  get '/classes/index' => 'classes/index'
+  get '/' => 'classes#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
