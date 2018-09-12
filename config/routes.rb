01e0bc5facd9ccb02bfadca00f8f1c1d28764942
sahoI <saho0003@gmail.com>
2018-09-12 18:44:33 +0900

    それぞれの曜日ごとに登録できるようにした 新しいアクションファイルの追加とルートの記述を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'registration/index' => 'registration#index'
+  get 'registration/mon' => 'registration#mon'
+  get 'registration/tues' => 'registration#tues'
+  get 'registration/wednes' => 'registration#wednes'
+  get 'registration/thurs' => 'registration#thurs'
+  get 'registration/fri' => 'registration#fri'
+  get 'registration/satur' => 'registration#satur'
   get '/' => 'home#index'
   get 'wethreapp/index' => 'wethreapp#index'
   get 'wethreapp/show' => 'wethreapp#show'
