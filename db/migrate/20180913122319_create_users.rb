31144d6f699b0a43e4766a8d4846cc4eeef94160
sahoI <saho0003@gmail.com>
2018-09-13 23:46:13 +0900

    usersテーブルにカラムを追加した

--- db/migrate/20180913122319_create_users.rb
+++ db/migrate/20180913122319_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :mail
+      t.string :department
+      t.string :friend_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_112919) do
+ActiveRecord::Schema.define(version: 2018_09_13_133145) do
 
   create_table "hoges", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2018_09_13_112919) do
   end
 
   create_table "registraters", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "department"
+    t.string "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
