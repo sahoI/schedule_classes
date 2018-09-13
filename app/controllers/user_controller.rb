f49158045c7dcf6c1e1e2190dbc059424d37443c
sahoI <saho0003@gmail.com>
2018-09-13 23:49:22 +0900

    ルーティングの追加とnewで新規登録したデータをcreateで保存しshowですべて呼び出している validationはまだかけていない

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,24 @@
 class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(post_params)
+    # render plain: params[:user].inspect
+    @user.save
+      # flash[:notice] = "登録しました"
+    redirect_to("/user/show")
+    # else
+    #   # flash[:notice] = "登録できませんでした"
+    #   render("/user/new")
+    # end
+  end
+  def post_params
+    params.require(:user).permit(:name, :email, :department, :friend_id)
+  end
 end
