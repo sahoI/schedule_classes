85a614e2d922e08614981a20d5f4f9aaf5a31e49
sahoI <saho0003@gmail.com>
2018-09-15 16:03:42 +0900

    usersテーブルのname,email,friend_idにバリデーションを作成 バリデーションによって保存されていないのを確認した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, {presence: true, uniqueness: true}
+  # https://qiita.com/shunhikita/items/772b81a1cc066e67930e 参照
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
+  validates :friend_id, {presence: true, uniqueness: true}
 end
