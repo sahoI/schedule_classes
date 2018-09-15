b5d4fcc20415d10a6869085de89ea89ca9045038
sahoI <saho0003@gmail.com>
2018-09-16 00:52:34 +0900

    registerでsearchとスクレイピングの実装途中まだ不十分

--- app/models/register.rb
+++ app/models/register.rb
@@ -1,2 +1,9 @@
 class Register < ApplicationRecord
+  def self.search(search)
+    if search
+      Register.where(['content LIKE ?', "%#{search}"])
+    else
+      Register.all
+    end
+  end
 end
