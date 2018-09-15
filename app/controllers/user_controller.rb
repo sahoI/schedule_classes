5dc8df52a940301890ddce68f91d16f9e03f2826
sahoI <saho0003@gmail.com>
2018-09-15 16:05:08 +0900

    バリデーションによって保存されないようにできたが、エラーメッセージは出ない

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,13 +10,13 @@ class UserController < ApplicationController
   def create
     @user = User.new(post_params)
     # render plain: params[:user].inspect
-    @user.save
-      # flash[:notice] = "登録しました"
-    redirect_to("/user/show")
-    # else
-    #   # flash[:notice] = "登録できませんでした"
-    #   render("/user/new")
-    # end
+    if @user.save
+      flash[:notice] = "登録しました"
+      redirect_to("/user/show")
+    else
+      flash[:notice] = "登録できませんでした"
+      render("/user/new")
+    end
   end
   def post_params
     params.require(:user).permit(:name, :email, :department, :friend_id)
