6d10071805ef0f375da9bf454ce1f1049bd7128a
sahoI <saho0003@gmail.com>
2018-09-15 16:42:36 +0900

    flash[:notice]でエラーを表示するのはとりあえず中止 英語だがエラー表示はできた

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,14 +11,14 @@ class UserController < ApplicationController
     @user = User.new(post_params)
     # render plain: params[:user].inspect
     if @user.save
-      flash[:notice] = "登録しました"
+      # flash[:notice] = "登録しました"
       redirect_to("/user/show")
     else
-      flash[:notice] = "登録できませんでした"
+      # flash[:notice] = "登録できませんでした"
       render("/user/new")
     end
   end
   def post_params
-    params.require(:user).permit(:name, :email, :department, :friend_id)
+    params.require(:user).permit(:name, :email, :department, :grade, :friend_id)
   end
 end
