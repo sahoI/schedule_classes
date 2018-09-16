531db354a755205a6d9bcd319d96770bf3f06178
sahoI <saho0003@gmail.com>
2018-09-16 18:50:38 +0900

    不要ファイルの削除など、整えた

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -16,27 +16,4 @@ class RegistrationController < ApplicationController
   def post_params
     params.require(:registrate).permit(:name)
   end
-  def show
-    @classes = Register.search(params[:num])
-
-    require 'nokogiri'
-    require 'open-uri'
-
-    url = 'https://syllabus.kyoto-su.ac.jp/syllabus/html/2018/3781.html'
-
-    charset = nil
-    html = open(url) do |f|
-      charset = f.charset # 文字種別を取得
-      f.read # htmlを読み込んで変数htmlに渡す
-    end
-
-    # ノコギリを使ってhtmlを解析
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-    @title = p doc.xpath('//td[@class="syllabus_item_left syllabus_frame_TRB"]/span[@class="font_data"]').text
-    @teacher = p doc.xpath('//td[@class="syllabus_item_left syllabus_frame_RB"]/span[@class="font_data"]').text
-    tds = doc.xpath('//td[@class="syllabus_item_left syllabus_frame_LRB space_top_bottom"]')
-    @value = p tds[6].text
-    @textbook = p tds[7].text
-
-  end
 end
