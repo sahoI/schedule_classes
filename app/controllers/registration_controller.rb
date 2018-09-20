5a7220653ec51a4a36b3cb220a1651cc64c26164
sahoI <saho0003@gmail.com>
2018-09-20 21:56:01 +0900

    不要なファイルの削除と登録画面、その後登録情報を順に表示するのを実装

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,19 +1,26 @@
 class RegistrationController < ApplicationController
   def index
-    @class = Register.find_by(id:1)
+    @courses = Course.all
+  end
+
+  def show
+    @time_schedules = Classlist.all
   end
 
   def create
-    @class = Register.new(post_params)
-    if @class.save
-      flash[:notice] = "登録しました"
-      redirect_to("/registration/index")
-    else
-      flash[:notice] = "登録できませんでした"
-      render("/registration/mon")
+    @class = Course.new(user_id: 1, class_id: params[:class_id], day_of_the_week: params[:day_of_the_week], time_schedule: params[:time_schedule])
+    if @class.class_id != nil
+      if @class.save
+        flash[:notice] = "登録しました"
+        redirect_to("/registration/index")
+      else
+        flash[:notice] = "登録できませんでした"
+        render("/classlist/show")
+      end
     end
   end
+
   def post_params
-    params.require(:registrate).permit(:name)
+    params.require(:course).permit(:class_id, :day_of_the_week, :time_schedule)
   end
 end
